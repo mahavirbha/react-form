df840af325d61d0bdf0a93d8c4f25581e77fb398
Mahavirbha <robowithsomeaix@gmail.com>
2021-12-18 01:23:39 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import "./App.css"
 
-function App() {
+export default function App() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const data = new FormData(event.currentTarget);
+    const values = Object.fromEntries(data.entries());
+
+    if (!Number(values.age)) {
+      alert('Age must be a number');
+      return;
+    }
+
+    if (!Number(values.weight)) {
+      alert('Weight must be a number');
+      return;
+    }
+
+    console.log('submitted', values);
+    console.log(JSON.stringify(values));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <h1>Hi!</h1>
+      
+      <p className='inp'>Enter your pet name:</p>
+      <input type="text" name="username" />
+
+      <p className='inp'>Enter pet's age:</p>
+      <input type="text" name="age" />
+
+      <p className='inp'>Enter pet's weight (Kg):</p>
+      <input type="text" name="weight" />
+
+      <p className='inp'>Enter pet type:</p>
+      <input type="text" name="type" />
+
+      <br /><br />
+      <input type="submit" />
+    </form>
   );
 }
-
-export default App;
